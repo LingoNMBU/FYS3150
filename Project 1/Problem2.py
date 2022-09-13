44dc1039f5a859c78130a072b6ed1f57661896ee
LingoNMBU <erlinge@nmbu,no>
2022-09-13 22:08:00 +0200

    Adding code for problem 2 and removed redundant script

--- Project 1/Problem2.py
+++ Project 1/Problem2.py
@@ -11,9 +11,11 @@ xu = pd.read_csv('x_u_exact', delim_whitespace=True)
 x = xu.iloc[:,0]
 u = xu.iloc[:,1]
 
-f, ax = plt.subplots(figsize=(12, 8), label= 'u(x)')
-ax.plot(x,u)
+f, ax = plt.subplots(figsize=(7, 5))
+ax.plot(x, u, label= 'u(x)')
 ax.set_title('Analytic Solution of the Poisson equation')
 ax.legend()
-
+plt.xlabel('x')
+plt.ylabel('u')
+plt.savefig('u_of_x.pdf')
 plt.show()
