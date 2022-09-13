78f412921b32185acd25bbae00a364b389941d02
LingoNMBU <erlinge@nmbu,no>
2022-09-13 23:36:53 +0200

    recommit

--- Project 1/Errors.py
+++
@@ -1,5 +0,0 @@
-# !python
-# -*- coding: utf-8 -*
-
-__author__ = 'Erling Ween Eriksen'
-__email__ = 'erlinge@nmbu.no'
--- Project 1/Plotting.py
+++ Project 1/Plotting.py
@@ -11,7 +11,7 @@ import pyarma as arma
 import sys
 import os
 
-working_dir = os.getcwd()
+#1. Importing data
 
 x10 = pd.read_csv(r'Prob7_x10')
 u10 = pd.read_csv(r'Prob7_u10')
@@ -48,27 +48,32 @@ u107 = pd.read_csv(r'Prob7_u10000000')
 v107 = pd.read_csv(r'Prob7_v_gen10000000')
 t107 = pd.read_csv(r'Prob7_time10000000')
 
-npoints = [10, 10e2, 10e3, 10e4, 10e5, 10e6, 10e7]
-x_values = [x10, x102, x103, x104, x105, x106, x107]
-u_values = [u10, u102, u103, u104, u105, u106, u107]
-v_values = [v10, v102, v103, v104, v105, v106, v107]
+npoints = [10, 10e2, 10e3, 10e4, 10e5]
+x_values = [x10, x102, x103, x104, x105]
+u_values = [u10, u102, u103, u104, u105]
+v_values = [v10, v102, v103, v104, v105]
 
-
-# f10, ax10 = plt.subplots(figsize=(12, 8))
-# ax10.set_ylim(0, 0.7)
-# for i in range(len(u_values)):
-#
-#    ax10.plot(x_values[i].values, u_values[i].values, label=f'exact solution {int(npoints[i])} points')
-#    ax10.plot(x_values[i].values, v_values[i].values, label=f'algorithm solution {int(npoints[i])} points', linestyle='dashed')
-#
-# ax10.set_title('Exact vs. algorithm')
-# ax10.legend()
-# plt.xlabel('x-value')
-# plt.ylabel('u-value')
-# plt.show()
+# Plotting solutions
+f1, ax1 = plt.subplots(figsize=(12, 8))
+ax1.set_ylim(0, 0.7)
+for i in range(len(u_values)):
+    ax1.plot(x_values[i].values, u_values[i].values, label=f'exact solution {int(npoints[i])} points')
+    ax1.plot(x_values[i].values, v_values[i].values, label=f'algorithm solution {int(npoints[i])} points', linestyle='dashed')
+ax1.set_title('Exact vs. algorithm')
+ax1.legend()
+plt.xlabel('x-value')
+plt.ylabel('u-value')
+plt.savefig('solution_plots.pdf')
+plt.show()
 
 
 def log_error(u, v):
+    """
+    Calculate logarithmic error
+    :param u: exact solution
+    :param v: model solution
+    :return:
+    """
     errors = np.zeros(len(u))
     k = 0
     for i, j in zip(u, v):
@@ -82,6 +87,12 @@ def log_error(u, v):
 
 
 def rel_error(u, v):
+    """
+    Calculates relative error
+    :param u: exact value
+    :param v: model value
+    :return:
+    """
     errors = np.zeros(len(u))
     k = 0
     for i, j in zip(u, v):
@@ -94,77 +105,51 @@ def rel_error(u, v):
             k += 1
     return errors
 
-
+# error calc points
 npoints2 = [10, 10e2, 10e3, 10e4, 10e5, 10e6]
 x_values2 = [x10, x102, x103, x104, x105, x106]
 u_values2 = [u10, u102, u103, u104, u105, u106]
 v_values2 = [v10, v102, v103, v104, v105, v106]
 
-#npoints2 = [10, 10e2, 10e3]
-#x_values2 = [x10, x102, x103]
-#u_values2 = [u10, u102, u103]
-#v_values2 = [v10, v102, v103]
+# max error points
+# npoints2 = [10, 10e2, 10e3, 10e4, 10e5, 10e6, 10e7]
+# x_values2 = [x10, x102, x103, x104, x105, x106, x107]
+# u_values2 = [u10, u102, u103, u104, u105, u106, u107]
+# v_values2 = [v10, v102, v103, v104, v105, v106, v107]
 
+# npoints2 = [10, 10e2, 10e3]
+# x_values2 = [x10, x102, x103]
+# u_values2 = [u10, u102, u103]
+# v_values2 = [v10, v102, v103]
 
+# plotting log error
 f2, ax2 = plt.subplots(figsize=(12, 8))
 for i in range(len(u_values2)):
     ax2.plot(x_values2[i].values[0:-1], log_error(u_values2[i].values[0:-1], v_values2[i].values[0:-1]),
-              label=f'{int(npoints[i])} points', linestyle='dashed')
+              label=f'{int(npoints2[i])} points', linestyle='dashed')
+ax2.set_title('Logarithmic errors')
+ax2.legend()
+plt.xlabel('x-value')
+plt.ylabel('log(Error)')
+plt.savefig('log_errors.pdf')
+plt.show()
 
-
+# plotting relative error and calculating max relative error
+max_relative_errors = {}
 f3, ax3 = plt.subplots(figsize=(12, 8))
 for i in range(len(u_values2)):
-    ax3.plot(x_values2[i].values[0:-1], rel_error(u_values2[i].values[0:-1], v_values2[i].values[0:-1]),
-              label=f'{int(npoints[i])} points', linestyle='dashed')
+    rel_error_i = rel_error(u_values2[i].values[0:-1], v_values2[i].values[0:-1])
+    ax3.plot(x_values2[i].values[0:-1], rel_error_i,
+             label=f'{int(npoints2[i])} points', linestyle='dashed')
 
+    max_relative_errors[f'{npoints2[i]}points'] = np.max(rel_error_i)
 ax3.set_title('Relative errors')
 ax3.legend()
 plt.xlabel('x-value')
 plt.ylabel('log(Error)')
+plt.savefig('rel_errors.pdf')
 plt.show()
 
-logerrors10 = log_error(fvals10, fvals10_alg)
-logerrors100 = log_error(fvals100, fvals100_alg)
-logerrors1000 = log_error(fvals1000, fvals1000_alg)
-logerrors100000 = log_error(fvals100000, fvals100000_alg)
-
-relerrors10 = rel_error(fvals10, fvals10_alg)
-relerrors100 = rel_error(fvals100, fvals100_alg)
-relerrors1000 = rel_error(fvals1000, fvals1000_alg)
-relerrors100000 = rel_error(fvals100000, fvals100000_alg)
-
-f210, ax10 = plt.subplots(figsize=(12, 8))
-ax10.plot(xvals10, logerrors10, label='logarithmic error')
-ax10.plot(xvals10, relerrors10, label='logarithmic relative error', linestyle='dashed')
-ax10.set_title('10 points')
-ax10.legend()
-plt.xlabel('x-value')
-plt.ylabel('Error')
-plt.show()
-
-f2100, ax2100 = plt.subplots(figsize=(12, 8))
-ax2100.plot(xvals100, logerrors100, label='logarithmic error')
-ax2100.plot(xvals100, relerrors100, label='logarithmic relative error', linestyle='dashed')
-ax2100.set_title('100 points')
-ax2100.legend()
-plt.xlabel('x-value')
-plt.ylabel('Error')
-plt.show()
-
-f21000, ax21000 = plt.subplots(figsize=(12, 8))
-ax21000.plot(xvals1000, logerrors1000, label='logarithmic error')
-ax21000.plot(xvals1000, relerrors1000, label='logarithmic relative error', linestyle='dashed')
-ax21000.set_title('1000 points')
-ax21000.legend()
-plt.xlabel('x-value')
-plt.ylabel('Error')
-plt.show()
-
-f21000000, ax2100000 = plt.subplots(figsize=(12, 8))
-ax2100000.plot(xvals100000, logerrors100000, label='logarithmic error')
-ax2100000.plot(xvals100000, relerrors100000, label='logarithmic relative error', linestyle='dashed')
-ax2100000.set_title('100000 points')
-ax2100000.legend()
-plt.xlabel('x-value')
-plt.ylabel('Error')
-plt.show()
+# saving max error file
+max_rel_err_df = pd.DataFrame(max_relative_errors, index=['max_errors'])
+max_rel_err_df.to_csv('max_rel_err.txt', sep='\t')
